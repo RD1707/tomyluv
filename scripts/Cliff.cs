d11f5ed6a2dec37cf8794283b9828559923b33b5
RD1707 <souplemonpie@gmail.com>
2025-06-07 01:08:07 -0300

    update

--- scripts/Cliff.cs
+++ scripts/Cliff.cs
@@ -4,15 +4,15 @@ namespace Alpheratz;
 // Teleport player to a specific location.
 public partial class Cliff : Area2D
 {
-    [Export]
-    public Node2D respawnLocation;
+	[Export]
+	public Node2D respawnLocation;
 
 	public override void _Ready()
 	{
-        BodyEntered += (Node2D body) => {
-            PlatformerPlayer player = body as PlatformerPlayer;
-            player.GlobalPosition = respawnLocation.GlobalPosition;
-            player.Hurt(0.0f, 100.0f);
-        };
+		BodyEntered += (Node2D body) => {
+			PlatformerPlayer player = body as PlatformerPlayer;
+			player.GlobalPosition = respawnLocation.GlobalPosition;
+			player.Hurt(0.0f, 100.0f);
+		};
 	}
 }
